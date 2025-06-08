bccf9f97634957f88dcd7cbb08a2edaf143be30b
poojadassani <poojadassani2811@gmail.com>
2025-06-08 15:53:48 +0530

    as

--- app.mjs
+++ app.mjs
@@ -15,7 +15,7 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
-  path: "/oarchat/socket.io/"
+  path: "socket.io/"
 });
 
 const dataFile = 'userData.json';
